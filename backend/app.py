9bac63779b5635ff3fa05d14bbce346f55af9878
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-12-15 17:53:01 +0100

    feat : get data with api in backend and create calculate energy function and return data

--- backend/app.py
+++ backend/app.py
@@ -4,15 +4,23 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify({"message": "Test Python!"})
-
-@app.route('/api/process', methods=['POST'])
+@app.route('/calculate-energy', methods=['POST'])
 def process_data():
     data = request.json
-    result = f"Processed: {data['input']}"
-    return jsonify({"message": result})
+    result = calculate_energy(data['electricity'],data['gas'],data['fuel'],12)
+    return jsonify(result)
+
+
+
+def calculate_energy(electricity,gas,fuel,month):
+    electricity_usage = electricity * month * 0.0005
+    gas_usage = gas * month * 0.0053
+    fuel_usage = fuel * month * 2.32
+    total_usage = electricity_usage + gas_usage + fuel_usage
+    print(electricity , electricity_usage)
+    return {"electricity_usage" : electricity_usage , "gas_usage" : gas_usage , "fuel_usage":fuel_usage,"total_usage":total_usage }
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
