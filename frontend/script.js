f39ff03d89da2750506485eaa4b97e896c4a4d45
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-12-15 17:51:30 +0100

    feat : add submit function for energy usage in script.js to send inputs data to backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,16 @@
-alert("Test Script")
+function submitEnergy() {
+  const data = {
+    electricity: Number(document.getElementById("electricity").value),
+    gas: Number(document.getElementById("gas").value),
+    fuel: Number(document.getElementById("fuel").value),
+  };
+  fetch("http://127.0.0.1:5000/calculate-energy", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+    });
+}
