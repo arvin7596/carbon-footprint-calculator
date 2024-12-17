5d5d793c529d5eb514887df8d4366eb88d867fbf
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-12-17 20:13:33 +0100

    feat : submit and get calculated data from waste api and update waste tab charts

--- frontend/script.js
+++ frontend/script.js
@@ -11,9 +11,37 @@ function submitEnergy() {
   })
     .then((res) => res.json())
     .then((result) => {
-      energyChart.data.datasets[0].data = [result.electricity_usage];
-      energyChart.data.datasets[1].data = [result.gas_usage];
-      energyChart.data.datasets[2].data = [result.fuel_usage];
-      energyChart.update();
+      energyConsumptionChart.data.datasets[0].data = [result.electricity_usage];
+      energyConsumptionChart.data.datasets[1].data = [result.gas_usage];
+      energyConsumptionChart.data.datasets[2].data = [result.fuel_usage];
+      energyConsumptionChart.update();
+      totalEnergyConsumptionChart.data.datasets[0].data = [
+        result.electricity_usage,
+      ];
+      totalEnergyConsumptionChart.data.datasets[1].data = [result.gas_usage];
+      totalEnergyConsumptionChart.data.datasets[2].data = [result.fuel_usage];
+      totalEnergyConsumptionChart.update();
+    });
+}
+
+function submitWaste() {
+  const data = {
+    waste: Number(document.getElementById("waste-generated").value),
+    recycle: Number(document.getElementById("recycle").value),
+  };
+  fetch("http://127.0.0.1:5000/calculate-waste", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+
+      wasteRecycleChart.data.datasets[0].data = [result.waste_generated];
+      wasteRecycleChart.data.datasets[1].data = [result.waste_recycled];
+      wasteRecycleChart.update();
+      remainedWasteChart.data.datasets[0].data = [result.waste_remained];
+      remainedWasteChart.update();
     });
 }
