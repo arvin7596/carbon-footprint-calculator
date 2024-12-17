126f0889f0b78ab268135e54276c060c213a54af
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-12-17 21:03:24 +0100

    feat : add travel emission chart

--- frontend/chart.js
+++ frontend/chart.js
@@ -140,3 +140,53 @@ const remainedWasteChart = new Chart(remainedWasteChartElement, {
     scales: { x: { stacked: false }, y: { stacked: false } },
   },
 });
+
+//----------------------------- Travel Charts -----------------------------
+
+// ----Bar Chart----
+
+const travelEmissionChartElement = document
+  .getElementById("travelEmissionChartElement")
+  .getContext("2d");
+const travelEmissionChart = new Chart(remainedWasteChartElement, {
+  type: "bar",
+  data: {
+    labels: ["Travel"],
+    datasets: [{ label: "Travel", data: [0], backgroundColor: "#c3c3c3" }],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      title: { display: true, text: "Travel" },
+      tooltip: { mode: "index", intersect: false },
+      legend: { position: "top" },
+    },
+    responsive: true,
+    scales: { x: { stacked: false }, y: { stacked: false } },
+  },
+});
+
+//----------------------------- Total Emission Charts -----------------------------
+
+// const wastePieChart = document.getElementById("wastePieChart").getContext("2d");
+// new Chart(wastePieChart, {
+//   type: "pie",
+//   data: {
+//     labels: ["Waste", "Recycled/Composted"],
+//     datasets: [
+//       {
+//         data: [60, 40],
+//         backgroundColor: ["#0384ff", "#007726"],
+//         borderWidth: 1,
+//       },
+//     ],
+//   },
+//   options: {
+//     plugins: {
+//       title: { display: true, text: "Waste vs Recycle" },
+//       tooltip: { mode: "index", intersect: false },
+//       legend: { position: "top" },
+//     },
+//     responsive: true,
+//   },
+// });
