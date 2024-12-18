283a61df3b9267ac9d3959c0ad1cadd8d32410d8
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-12-18 16:03:16 +0100

    feat : add report tab and table and button for generating PDF

--- frontend/script.js
+++ frontend/script.js
@@ -58,7 +58,60 @@ function submitTravel() {
   })
     .then((res) => res.json())
     .then((result) => {
-      travelEmissionChart.data.datasets[0].data = [result.co2_business_travel];
+      travelEmissionChart.data.datasets[0].data = [
+        result.business_travel_emission,
+      ];
       travelEmissionChart.update();
     });
+}
+
+function getReportData() {
+  fetch("http://127.0.0.1:5000/get_report_data", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      totalEmissionChart.data.datasets[0].data = [
+        result.electricity_emission,
+        result.gas_emission,
+        result.fuel_emission,
+        result.waste_emission,
+        result.business_travel_emission,
+      ];
+      totalEmissionChart.update();
+      document.getElementById("electricity-table").textContent =
+        result.electricity_emission;
+      document.getElementById("gas-table").textContent = result.gas_emission;
+      document.getElementById("fuel-table").textContent = result.fuel_emission;
+      document.getElementById("waste-table").textContent =
+        result.waste_emission;
+      document.getElementById("travel-table").textContent =
+        result.business_travel_emission;
+      document.getElementById("total-table").textContent =
+        result.total_emission;
+    });
+}
+
+function init() {
+  fetch("http://127.0.0.1:5000/init");
+}
+window.onload = init;
+
+function createPDF() {
+  fetch("http://127.0.0.1:5000/generate_pdf")
+    .then((response) => {
+      if (response.ok) {
+        return response.blob();
+      }
+      throw new Error("Failed to generate PDF");
+    })
+    .then((blob) => {
+      // Create a link element to trigger the download
+      const link = document.createElement("a");
+      link.href = window.URL.createObjectURL(blob);
+      link.download = "carbon_footprint_report.pdf"; // Set the file name
+      link.click(); // Trigger the download
+    })
+    .catch((error) => console.error("Error:", error));
 }
