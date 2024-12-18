b6a44af2ca4f89dc01502445376aa035ef5c3814
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-12-18 15:37:04 +0100

    feat : add total emission pie chart

--- frontend/chart.js
+++ frontend/chart.js
@@ -168,25 +168,48 @@ const travelEmissionChart = new Chart(travelEmissionChartElement, {
 
 //----------------------------- Total Emission Charts -----------------------------
 
-// const wastePieChart = document.getElementById("wastePieChart").getContext("2d");
-// new Chart(wastePieChart, {
-//   type: "pie",
-//   data: {
-//     labels: ["Waste", "Recycled/Composted"],
-//     datasets: [
-//       {
-//         data: [60, 40],
-//         backgroundColor: ["#0384ff", "#007726"],
-//         borderWidth: 1,
-//       },
-//     ],
-//   },
-//   options: {
-//     plugins: {
-//       title: { display: true, text: "Waste vs Recycle" },
-//       tooltip: { mode: "index", intersect: false },
-//       legend: { position: "top" },
-//     },
-//     responsive: true,
-//   },
-// });
+const totalEmissionChartElement = document
+  .getElementById("total-emission-chart-element")
+  .getContext("2d");
+const data = {
+  labels: ["Electricity", "Gas", "Fuel", "Waste", "Business Travel"],
+  datasets: [
+    {
+      data: [0, 0, 0, 0, 0],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.5)",
+        "rgba(54, 162, 235, 0.5)",
+        "rgba(255, 206, 86, 0.5)",
+        "rgba(75, 192, 192, 0.5)",
+        "rgba(153, 102, 255, 0.5)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Total Emission(kgCO2)",
+    },
+    legend: {
+      position: "right",
+    },
+  },
+  responsive: true,
+};
+
+const totalEmissionChart = new Chart(totalEmissionChartElement, {
+  type: "pie",
+  data: data,
+  options: options,
+});
