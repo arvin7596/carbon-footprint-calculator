e915f11072622a06f0c364f4ab37423d4d8b3e69
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-12-19 16:08:20 +0100

    refactor : handle error with try and catch

--- frontend/script.js
+++ frontend/script.js
@@ -1,104 +1,136 @@
-function submitEnergy() {
+async function submitEnergy() {
+  //Get Energy Usage Data from Inputs
   const data = {
     electricity: Number(document.getElementById("electricity").value),
     gas: Number(document.getElementById("gas").value),
     fuel: Number(document.getElementById("fuel").value),
   };
-  fetch("http://127.0.0.1:5000/calculate-energy", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((result) => {
-      energyConsumptionChart.data.datasets[0].data = [result.electricity_usage];
-      energyConsumptionChart.data.datasets[1].data = [result.gas_usage];
-      energyConsumptionChart.data.datasets[2].data = [result.fuel_usage];
-      energyConsumptionChart.update();
-      totalEnergyConsumptionChart.data.datasets[0].data = [
-        result.electricity_usage,
-      ];
-      totalEnergyConsumptionChart.data.datasets[1].data = [result.gas_usage];
-      totalEnergyConsumptionChart.data.datasets[2].data = [result.fuel_usage];
-      totalEnergyConsumptionChart.update();
+  // Call Api to calculate Data
+  try {
+    const response = await fetch("http://127.0.0.1:5000/calculate-energy", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
     });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error);
+    }
+    const result = await response.json();
+    energyConsumptionChart.data.datasets[0].data = [result.electricity_usage];
+    energyConsumptionChart.data.datasets[1].data = [result.gas_usage];
+    energyConsumptionChart.data.datasets[2].data = [result.fuel_usage];
+    energyConsumptionChart.update();
+    totalEnergyConsumptionChart.data.datasets[0].data = [
+      result.electricity_usage,
+    ];
+    totalEnergyConsumptionChart.data.datasets[1].data = [result.gas_usage];
+    totalEnergyConsumptionChart.data.datasets[2].data = [result.fuel_usage];
+    totalEnergyConsumptionChart.update();
+  } catch (error) {
+    console.log("error", error);
+    alert(error);
+  }
 }
 
-function submitWaste() {
+async function submitWaste() {
+  //Get Waste Data from Inputs
   const data = {
     waste: Number(document.getElementById("waste-generated").value),
     recycle: Number(document.getElementById("recycle").value),
   };
-  fetch("http://127.0.0.1:5000/calculate-waste", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((result) => {
-      console.log(result);
-
-      wasteRecycleChart.data.datasets[0].data = [result.waste_generated];
-      wasteRecycleChart.data.datasets[1].data = [result.waste_recycled];
-      wasteRecycleChart.update();
-      remainedWasteChart.data.datasets[0].data = [result.waste_remained];
-      remainedWasteChart.update();
+  try {
+    const response = await fetch("http://127.0.0.1:5000/calculate-waste", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
     });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error);
+    }
+    const result = await response.json();
+    wasteRecycleChart.data.datasets[0].data = [result.waste_generated];
+    wasteRecycleChart.data.datasets[1].data = [result.waste_recycled];
+    wasteRecycleChart.update();
+    remainedWasteChart.data.datasets[0].data = [result.waste_remained];
+    remainedWasteChart.update();
+  } catch (error) {
+    console.log("error", error);
+    alert(error);
+  }
 }
 
-function submitTravel() {
+async function submitTravel() {
+  //Get Travel Business Data from Inputs
   const data = {
     traveled: Number(document.getElementById("kilometers-traveled").value),
     vehicles: Number(document.getElementById("vehicles-fuel").value),
   };
-  fetch("http://127.0.0.1:5000/calculate-travel", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((result) => {
-      travelEmissionChart.data.datasets[0].data = [
-        result.business_travel_emission,
-      ];
-      travelEmissionChart.update();
+  try {
+    const response = await fetch("http://127.0.0.1:5000/calculate-travel", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
     });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error);
+    }
+    const result = await response.json();
+    travelEmissionChart.data.datasets[0].data = [
+      result.business_travel_emission,
+    ];
+    travelEmissionChart.update();
+  } catch (error) {
+    console.log("error", error);
+    alert(error);
+  }
 }
 
-function getReportData() {
-  fetch("http://127.0.0.1:5000/get_report_data", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((res) => res.json())
-    .then((result) => {
-      totalEmissionChart.data.datasets[0].data = [
-        result.electricity_emission,
-        result.gas_emission,
-        result.fuel_emission,
-        result.waste_emission,
-        result.business_travel_emission,
-      ];
-      totalEmissionChart.update();
-      document.getElementById("electricity-table").textContent =
-        result.electricity_emission;
-      document.getElementById("gas-table").textContent = result.gas_emission;
-      document.getElementById("fuel-table").textContent = result.fuel_emission;
-      document.getElementById("waste-table").textContent =
-        result.waste_emission;
-      document.getElementById("travel-table").textContent =
-        result.business_travel_emission;
-      document.getElementById("total-table").textContent =
-        result.total_emission;
+async function getReportData() {
+  //Get Report Data from Api and fill Chart and Table
+  try {
+    const response = await fetch("http://127.0.0.1:5000/get_report_data", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
     });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error);
+    }
+    const result = await response.json();
+    totalEmissionChart.data.datasets[0].data = [
+      result.electricity_emission,
+      result.gas_emission,
+      result.fuel_emission,
+      result.waste_emission,
+      result.business_travel_emission,
+    ];
+    totalEmissionChart.update();
+    document.getElementById("electricity-table").textContent =
+      result.electricity_emission;
+    document.getElementById("gas-table").textContent = result.gas_emission;
+    document.getElementById("fuel-table").textContent = result.fuel_emission;
+    document.getElementById("waste-table").textContent = result.waste_emission;
+    document.getElementById("travel-table").textContent =
+      result.business_travel_emission;
+    document.getElementById("total-table").textContent = result.total_emission;
+  } catch (error) {
+    console.log("error", error);
+    alert(error);
+  }
 }
 
 function init() {
+  // Reset Global values
   fetch("http://127.0.0.1:5000/init");
 }
 window.onload = init;
 
 function createPDF() {
+  // Create Report PDF
   fetch("http://127.0.0.1:5000/generate_pdf")
     .then((response) => {
       if (response.ok) {
